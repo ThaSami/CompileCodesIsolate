42430b11519a7c11abc5432c7cd40dd43c8cc44d
Sami Shabaneh <thasami97@gmail.com>
2019-11-10 21:32:11 +0200

    Update Assignment.java

--- Assignment.java
+++ Assignment.java
@@ -8,9 +8,9 @@ import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class Assignment {
-
+    //so many bad practices and dublicates ,but it does the job... 
     static void print(Process p1, Process p2,Statement stmt,long hash) throws IOException, SQLException {
-        String s = null;
+        String s = null;    
         BufferedReader stdInput = new BufferedReader(new
                 InputStreamReader(p1.getInputStream()));
 
@@ -57,6 +57,7 @@ public class Assignment {
         System.out.println("Here is the standard output of the command:\n");
         while ((s = stdInput.readLine()) != null) {
             System.out.println(s);
+            //insert the file hash and output to DATABASE.
             stmt.execute("INSERT INTO codes (hash, output) VALUES (\""+String.valueOf(hash)+"\",\""+s+"\");");
 
         }
@@ -84,28 +85,31 @@ public class Assignment {
 
 
         try {
+            //connect to database.
             Class.forName("com.mysql.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db?verifyServerCertificate=false&useSSL=true", "root", "password");
             Statement stmt = con.createStatement();
-            long hash = Objects.hashCode(readFile(args[0]));
+            long hash = Objects.hashCode(readFile(args[0])); //take the filhe HashCode for later reference
+            //check if the file is in the database
             ResultSet resultSet = stmt.executeQuery("select * from codes where hash=" +"\""+String.valueOf(hash)+"\"");
-
+            //is it in the database?
            if(resultSet.next()){
-               System.out.println("File output is : "+ resultSet.getString("output"));
-               System.exit(0);
+               System.out.println("File output is : "+ resultSet.getString("output")); //print that code output
+               System.exit(0); //exit
             }
-
+            //does the file end with CPP?
             if (args[0].endsWith(".cpp")) {
                 Process p = Runtime.getRuntime().exec("g++ " + args[0] + " -o out");
-                TimeUnit.SECONDS.sleep(3);
-                Process p2 = Runtime.getRuntime().exec("sh -c ./out");
-
-                print(p, p2,stmt,hash);
+                TimeUnit.SECONDS.sleep(3); //this is needed since containers are too slow to generate the out file
+                Process p2 = Runtime.getRuntime().exec("sh -c ./out"); // run the file
+                print(p, p2,stmt,hash); //print the file (so many dublicates i know)
+              
+              //ain't gonna explain this..
             } else if (args[0].endsWith(".py")) {
                 Process p = Runtime.getRuntime().exec("python " + args[0]);
                 print(p,stmt,hash);
             }
-            con.close();
+            con.close(); 
 
         } catch (Exception e) {
             System.out.println("exception happened - here's what I know: ");
